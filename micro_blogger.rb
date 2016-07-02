e9ee7bac533b24074bec68d1cfd1f53dd83fe05b
ob00042 <ob00042@gmail.com>
2016-07-02 18:39:13 +0300

    Iteration 2

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -13,10 +13,26 @@ class MicroBlogger
   	if message.length <= 140
   	  @client.update(message)
   	else
-  	  puts "The message is over 140 characters long. Will not tweet!"
+  	  puts "The message is over 140 characters long." 
+  	  puts "Will not tweet!"
   	end
+  end
 
-
+  def run
+  	puts "Welcome to the JSL Twitter Client!"
+  	command = ""
+  	while command != "q"
+      printf "Enter command: "
+      input = gets.chomp
+      parts = input.split(" ")
+      command = parts[0]
+      case command
+         when 'q' then puts "Goodbye!"
+         when 't' then self.tweet(parts[1..-1].join(" "))
+         else
+           puts "Sorry, I don't know how to #{command}"
+      end
+    end
 
   end
 
@@ -24,6 +40,4 @@ class MicroBlogger
 end
 
 blogger = MicroBlogger.new
-blogger.tweet("Less than 140")
-blogger.tweet("Exactly 140 characters. This is kinda stupid having to write all of it. Halfway through. Let's go! I got this. I ate late today, and I willa")
-blogger.tweet("More than 140 characters. I am not sure how long this should be. Kinda bored to type actual things, so... jachpodjcskdbcsodich[doockkjceiycgwpcokwclkndckhbiuchepock[ckelfjvnefivbefpvjfghjkjhghjk")
+blogger.run
